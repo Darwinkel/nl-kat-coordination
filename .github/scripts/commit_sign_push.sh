90b5d2947aa4eaea1142cbe1c301c7df7869201b
Patrick <Darwinkel@users.noreply.github.com>
2023-07-10 13:18:01 +0200

    Update commit_sign_push.sh

--- .github/scripts/commit_sign_push.sh
+++ .github/scripts/commit_sign_push.sh
@@ -17,4 +17,4 @@ for FILE in $FILES; do
 done
 
 # Make sure the required checks pass
-gh workflow run pre_commit_checks.yml --ref "$COMMIT"
+gh workflow run pre_commit_checks.yml -f commit_sha="$COMMIT"
